f8c503c8c2bf3f0f6d522be089a0823eff63f973
Adiansyah Putra <adiansyahhputra@gmail.com>
2022-11-28 07:13:15 +0700

    alhamdulillah

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+
+import Button from './components/UI/Button/Button';
 
 import './App.css';
+import DemoOutput from './components/Demo/DemoOutput';
 
 function App() {
+  const [showParagraf, setShowParagraf] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
+
+  console.log('APP RUNNING');
+
+  const toggleParagragfHandler = useCallback(() => {
+    if (allowToggle) {
+      setShowParagraf((prefShowParagraph) => !prefShowParagraph);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DemoOutput show={showParagraf} />
+      <Button onClick={allowToggleHandler}>Allow Toggling</Button>
+      <Button onClick={toggleParagragfHandler}>Toggle Paragraph!</Button>
     </div>
   );
 }
