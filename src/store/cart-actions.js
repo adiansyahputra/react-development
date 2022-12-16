19191fe1d29ad849fb709d304c456858e419472a
Adiansyah Putra <adiansyahhputra@gmail.com>
2022-12-16 11:50:51 +0700

    alhamdulillah

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -4,9 +4,7 @@ import { cartActions } from './cart-slice';
 export const fetchCartData = () => {
   return async (dispatch) => {
     const fetchData = async () => {
-      const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/cart.json'
-      );
+      const response = await fetch('https://react-http-b95f2-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json');
 
       if (!response.ok) {
         throw new Error('Could not fetch cart data!');
@@ -48,16 +46,13 @@ export const sendCartData = (cart) => {
     );
 
     const sendRequest = async () => {
-      const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/cart.json',
-        {
-          method: 'PUT',
-          body: JSON.stringify({
-            items: cart.items,
-            totalQuantity: cart.totalQuantity,
-          }),
-        }
-      );
+      const response = await fetch('https://react-http-b95f2-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json', {
+        method: 'PUT',
+        body: JSON.stringify({
+          items: cart.items,
+          totalQuantity: cart.totalQuantity,
+        }),
+      });
 
       if (!response.ok) {
         throw new Error('Sending cart data failed.');
